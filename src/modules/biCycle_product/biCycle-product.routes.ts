e8495f54bdab62193ae962c0b0c35c671ec4bf9f
Mahmud Preyom <preyom5076@example.com>
2025-01-28 12:12:58 +0600

    validation error add

--- src/modules/biCycle_product/biCycle-product.routes.ts
+++ src/modules/biCycle_product/biCycle-product.routes.ts
@@ -2,11 +2,20 @@ import { Router } from 'express';
 import { cycleController } from './biCycle-product.controller';
 import auth from '../../middlewares/auth';
 import { USER_ROLE } from '../users/user.const';
+import { BiCycleProductValidationSchema } from './biCycle-product.validation';
+import validateRequest from '../../middlewares/validateRequest';
 
 const BiCycleRouter = Router();
 
 // biCycleRouter.post('/biCycle-create', cycleController.createBiCycle);
-BiCycleRouter.post('/', auth(USER_ROLE.admin), cycleController.createBiCycle);
+BiCycleRouter.post(
+  '/',
+  auth(USER_ROLE.admin),
+  validateRequest(
+    BiCycleProductValidationSchema.createBiCycleProductValidationSchema,
+  ),
+  cycleController.createBiCycle,
+);
 BiCycleRouter.get('/:productId', cycleController.getSingleBiCycle);
 BiCycleRouter.patch(
   '/:productId',
---
+++ src/modules/biCycle_product/biCycle-product.validation.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+
+export const createBiCycleProductValidationSchema = z.object({
+  body: z.object({
+    name: z.string({ required_error: 'Name is required.' }),
+    brand: z.string({ required_error: 'Brand is required.' }),
+    price: z
+      .number({ required_error: 'Price is required.' })
+      .positive('Price must be a positive number.'),
+    type: z.enum(['Mountain', 'Road', 'Hybrid', 'BMX', 'Electric'], {
+      required_error: 'Type is required.',
+      invalid_type_error:
+        'Invalid type. Choose from Mountain, Road, Hybrid, BMX, or Electric.',
+    }),
+    description: z.string({ required_error: 'Description is required.' }),
+    quantity: z
+      .number({ required_error: 'Quantity is required.' })
+      .int('Quantity must be an integer.')
+      .nonnegative('Quantity must be a non-negative number.'),
+    inStock: z.boolean({ required_error: 'InStock status is required.' }),
+  }),
+});
+
+export const BiCycleProductValidationSchema = {
+  createBiCycleProductValidationSchema,
+};
